060160291636c7e1cb642e7ad1d53a8b73402c0d
fredrik <fbiten@kth.se>
2018-06-21 19:51:18 +0200

    Adding timer for game

--- app/src/main/java/com/fredrikbiten/animationer/BackgroundFragment.java
+++ app/src/main/java/com/fredrikbiten/animationer/BackgroundFragment.java
@@ -2,6 +2,7 @@ package com.fredrikbiten.animationer;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,22 @@ public class BackgroundFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.background_fragment,container,false);
         scoreNumber = (TextView) view.findViewById(R.id.score_number);
+
+        //Timer to display timeleft on gamescreen
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+
+            }
+
+            public void onFinish() {
+
+            }
+
+        }.start();
+
+
         return view;
     }
     public void updateScore(int score){
