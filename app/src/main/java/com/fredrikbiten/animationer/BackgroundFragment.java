9dfec79688133b439c70d5a109f9273627e7c0f2
fredrik <fbiten@kth.se>
2018-06-22 16:57:15 +0200

    Adding working timer + layout changes

--- app/src/main/java/com/fredrikbiten/animationer/BackgroundFragment.java
+++ app/src/main/java/com/fredrikbiten/animationer/BackgroundFragment.java
@@ -1,6 +1,7 @@
 package com.fredrikbiten.animationer;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
@@ -12,16 +13,21 @@ import android.widget.TextView;
 public class BackgroundFragment extends Fragment {
 
     private TextView scoreNumber;
+    private TextView displayTimer;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.background_fragment,container,false);
         scoreNumber = (TextView) view.findViewById(R.id.score_number);
-
+        displayTimer = (TextView) view.findViewById(R.id.displayTimer);
         //Timer to display timeleft on gamescreen
         new CountDownTimer(30000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-
+                //Check how long we got left and change color on text
+                if ((millisUntilFinished / 1000) < 15 && (millisUntilFinished / 1000) > 5)
+                    displayTimer.setTextColor(Color.YELLOW);
+                if ((millisUntilFinished / 1000) <= 5) displayTimer.setTextColor(Color.RED);
+                displayTimer.setText(Long.toString(millisUntilFinished / 1000));
 
             }
 
